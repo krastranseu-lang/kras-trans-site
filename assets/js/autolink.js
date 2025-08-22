567fb1b5b2ebd493c963b0cba9acc6992579fcb1
krastranseu-lang <krastranseu@gmail.com>
2025-08-22 02:06:13 +0200

    fix: wrap nav and tidy scripts

--- assets/js/autolink.js
+++
@@ -1,4 +0,0 @@
-// Ochrona na zewn. linki + proste autolinki
-document.querySelectorAll('a[href^="http"]').forEach(a=>{
-if (!a.href.includes(location.host)) a.setAttribute('rel','noopener noreferrer');
-});
